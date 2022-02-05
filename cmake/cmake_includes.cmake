58bc771c186d07f386317d18fa834f1b84ce1ba8
Matthew Tyler McGlawn <mtmcglawn@gmail.com>
2022-02-05 16:54:35 -0500

    Correct CMake for tdd
    This will correct a bug that was added when making sure that the program
    would compile without googletest.

--- cmake/cmake_includes.cmake
+++ cmake/cmake_includes.cmake
@@ -4,12 +4,16 @@ include(${CMAKE_DIR}/compiler.cmake)
 
 
 #######################################
+# Check that googletest is installed
+#######################################
+if(EXISTS ${GOOGLETEST_DIR}/CMakeLists.txt)
+	set(ENABLE_TESTS TRUE)
+endif()
+
+
+#######################################
 # Add External Dependencies
 #######################################
 if(NOT ${CMAKE_SOURCE_DIR} STREQUAL ${BASE_DIR})
 	add_subdirectory(${EXTERNAL_DEPS_DIR} ${BUILD_DIR}/external)
-endif()
-
-if(EXISTS ${GOOGLETEST_DIR}/CMakeLists.txt)
-	set(ENABLE_TESTS TRUE)
 endif()
